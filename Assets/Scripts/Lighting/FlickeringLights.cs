f0e7244af1c89d483c7cc38ab0a9a5863a183c90
Harry <profawesum@gmail.com>
2020-02-13 11:54:10 +1300

    Flickering Lights Script

---
+++ Assets/Scripts/Lighting/FlickeringLights.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLights : MonoBehaviour
+{
+
+    int timer;
+    Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        timer = Random.Range(0, 100000);
+
+        if (timer > 99000)
+        {
+            light.enabled = false;
+        }
+        if (timer < 99000) {
+            light.enabled = true;
+        }
+
+    }
+}
