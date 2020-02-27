65066241aad0b0e4fb7b67808bb7733f157ab1ff
Ryan <mstwilko@gmail.com>
2020-02-27 23:33:38 +1300

    Main Menu (Functionally Done)
    SFX To Be Added!

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Gets The Next Scene for the Play Mode
+        SceneManager.LoadScene(1);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
