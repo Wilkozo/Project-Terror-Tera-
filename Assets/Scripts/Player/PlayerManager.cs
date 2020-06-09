522850f6c2007df5c1f733f1b69aa897005ecda0
Harry <profawesum@gmail.com>
2020-06-09 14:17:29 +1200

    Updating to the latest version of desmonds

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -8,6 +8,8 @@ public class PlayerManager : MonoBehaviour
     public Text totalDocuments;
     public int documentCount;
 
+
+
     public void documentsPickedUp() {
         documentCount++;
         totalDocuments.text = "Documents: " + documentCount.ToString() + " / 5";
