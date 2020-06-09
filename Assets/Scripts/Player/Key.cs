dc0b4e1c43f41c689545cd9fefd9628a45dd8b6e
Harry <profawesum@gmail.com>
2020-06-10 10:26:38 +1200

    prefabs

---
+++ Assets/Scripts/Player/Key.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+
+    public Transform gateToMove;
+    public Vector3 whereToMoveGateTo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+
+            gateToMove.position = whereToMoveGateTo;
+            Destroy(this.gameObject);
+
+        }
+    }
+}
