1e88f559d1da89c61ec5a63a3407e2a82a4969e7
Harry <profawesum@gmail.com>
2020-06-10 18:18:42 +1200

    Audio Updates

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player") {
+            other.GetComponent<playerHealth>().health -= 0.5f * Time.deltaTime;
+        }
+    }
+
+}
