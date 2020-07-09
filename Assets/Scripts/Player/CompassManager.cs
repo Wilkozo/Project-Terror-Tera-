da4eefe0aa38430945e4e742ee144152ce64b937
Harry <profawesum@gmail.com>
2020-07-09 14:20:22 +1200

    Compass is on the map now

---
+++ Assets/Scripts/Player/CompassManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CompassManager : MonoBehaviour
+{
+
+    public RawImage compassImage;
+    public Transform player;
+
+    private void Update()
+    {
+        compassImage.uvRect = new Rect(player.localEulerAngles.y / 360f, 0f, 1f, 1f);
+    }
+
+}
