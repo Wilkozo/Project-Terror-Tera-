3f79c5ce0cca5aa762e5db9d58efc1d66b86d5b0
Harry <profawesum@gmail.com>
2020-07-13 11:14:54 +1200

    Work on the photography system

---
+++ Assets/Scripts/PhotoCamera/DinoChecklist.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoChecklist : MonoBehaviour
+{
+    //list of dinos to take photos of
+    public List<string> dinosaursPhotographed;
+
+    public void dinosPhotographed(string tagOfDino) {
+        Debug.Log(tagOfDino);
+        //checks through the array
+        for(int i = 0; i < dinosaursPhotographed.Count; i++) {
+            //if the dinosaur photographed is the same as the tag of the dino
+            if (dinosaursPhotographed[i] == tagOfDino) {
+                //remove it from the list of dinos
+                dinosaursPhotographed.Remove(tagOfDino);
+            }
+        }
+
+    }
+
+}
