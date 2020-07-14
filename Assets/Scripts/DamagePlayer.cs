b2063c2b4e51c4244952f19a541d9a5d928b5bfe
Harry <profawesum@gmail.com>
2020-07-15 11:39:21 +1200

    Death screen is now just an overlay, so it will reload faster now

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
-    public AudioSource source;
-    public AudioClip clip;
+    //public AudioSource source;
+    //public AudioClip clip;
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Player") {
-            other.GetComponent<playerHealth>().health -= 0.5f * Time.deltaTime;
-        }
-    }
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.tag == "Player") {
+    //        other.GetComponent<playerHealth>().health -= 0.5f * Time.deltaTime;
+    //    }
+    //}
 
-    private void Update()
-    {
-        float random = Random.Range(0, 1000);
+    //private void Update()
+    //{
+    //    float random = Random.Range(0, 1000);
 
-        if (random >= 999 && !source.isPlaying)
-        {
-            source.PlayOneShot(clip);
-        }
-    }
+    //    if (random >= 999 && !source.isPlaying)
+    //    {
+    //        source.PlayOneShot(clip);
+    //    }
+    //}
 
 }
--- Assets/Scripts/Player/playerHealth.cs
+++ Assets/Scripts/Player/playerHealth.cs
@@ -3,43 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-public class playerHealth : MonoBehaviour
+namespace UnityStandardAssets.Characters.FirstPerson
 {
-
-    public Image playerHealthImage;
-    public Image bloodOverlay;
-    public float health = 100;
-
-    public CanvasGroup canGroup;
-
-    public bool loseHealth;
-
-    private void Start()
+    public class playerHealth : MonoBehaviour
     {
-        canGroup.alpha = 0;
-       // health = 100;
-    }
 
-    private void Update()
-    {
-        health = playerHealthImage.fillAmount;
+        public Image playerHealthImage;
+        public Image bloodOverlay;
+        public float health = 100;
 
-        if (health <= 0)
+        public CanvasGroup canGroup;
+        public CanvasGroup gameOverCanGroup;
+
+        public bool loseHealth;
+
+        private void Start()
         {
-            //what to do when a player dies 
-            Application.LoadLevel("GameOver");
+            canGroup.alpha = 0;
+            gameOverCanGroup.alpha = 0;
+            // health = 100;
         }
-            playerHealthImage.fillAmount += 0.01f * Time.deltaTime;
-            canGroup.alpha -= 0.01f * Time.deltaTime;
-    }
 
-    public void OnTriggerEnter(Collider other)
-    {
-        //if the player hits the kill floor
-        if (other.tag == "KillFloor") {
-            //go to the gameover screen
-            Application.LoadLevel("Gameover");
+        private void Update()
+        {
+            health = playerHealthImage.fillAmount;
+
+            if (Input.GetKey(KeyCode.T))
+            {
+                playerHealthImage.fillAmount -= 0.1f * Time.deltaTime;
+                health -= 1.0f * Time.deltaTime;
+                canGroup.alpha += 0.5f * Time.deltaTime;
+            }
+            else if(health > 0)
+            {
+
+                playerHealthImage.fillAmount += 0.01f * Time.deltaTime;
+                canGroup.alpha -= 0.01f * Time.deltaTime;
+            }
+
+            if (health <= 0)
+            {
+                health = 0;
+                //what to do when a player dies 
+                gameOverCanGroup.alpha += 0.75f * Time.deltaTime;
+                this.gameObject.GetComponent<FirstPersonController>().enabled = false;
+                if (Input.GetKeyDown(KeyCode.R)) {
+                    Application.LoadLevel(Application.loadedLevel);
+                }
+                if (Input.GetKeyDown(KeyCode.Q)) {
+                    Application.LoadLevel("NewMainMenu");
+                }
+            }
+
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            //if the player hits the kill floor
+            if (other.tag == "KillFloor")
+            {
+                //go to the gameover screen
+                Application.LoadLevel("Gameover");
+            }
         }
     }
 }
