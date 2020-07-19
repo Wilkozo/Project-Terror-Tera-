93ad3dae5e67926e896eaef7c2bc5cf57c5b9bee
Harry <profawesum@gmail.com>
2020-07-20 10:36:31 +1200

    Raptor will flee the first time the player sees it

--- Assets/Scripts/ScriptableEvents/RaptorFlee.cs
+++ Assets/Scripts/ScriptableEvents/RaptorFlee.cs
@@ -6,11 +6,27 @@ using UnityEngine;
 //and they run from the player
 public class RaptorFlee : MonoBehaviour
 {
+    //get the WaypointNavigator script
+    [SerializeField] WaypointNavigator navigator;
+    public Waypoint newWaypoint;
+
+    private void Start()
+    {
+        navigator.enabled = true;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         //if the ai collides with the player
         if (other.tag == "Player") {
             //run away from the player into the forest
+            //set it so the raptors will follow through a path
+            navigator.currentWaypoint = newWaypoint;
+            //if there is no next waypoint
+            if (navigator.currentWaypoint == null) {
+                //Destroy the raptor
+                Destroy(this.gameObject);
+            }
 
         }
     }
