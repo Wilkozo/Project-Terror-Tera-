1196db915cce5285e1883449d46904a56e978923
Harry <profawesum@gmail.com>
2020-08-04 14:00:23 +1200

    More documents placed and a minor update to the sceneLoader

--- Assets/Scripts/Menus/SceneLoader.cs
+++ Assets/Scripts/Menus/SceneLoader.cs
@@ -17,35 +17,28 @@ public class SceneLoader : MonoBehaviour
     void LateUpdate()
     {
 
-        // ...set the loadScene boolean to true to prevent loading a new scene more than once...
+        //prevents reloading the same scene multiple times=
         loadScene = true;
 
-        // ...change the instruction text to read "Loading..."
+        //set the loading text
         loadingText.text = "Loading...";
 
-        // ...and start a coroutine that will load the desired scene.
+        //start the coroutine that loads the scene
         StartCoroutine(LoadNewScene());
 
-        // If the new scene has started loading...
+        // If the new scene has started loading
         if (loadScene == true)
         {
 
-            // ...then pulse the transparency of the loading text to let the player know that the computer is still working.
+            //loading text to phase in and out 
             loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
 
         }
 
     }
-
-
-    // The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
+    
     IEnumerator LoadNewScene()
     {
-
-        // This line waits for 3 seconds before executing the next line in the coroutine.
-        // This line is only necessary for this demo. The scenes are so simple that they load too fast to read the "Loading..." text.
-        //yield return new WaitForSeconds(2);
-
         // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
         AsyncOperation async = Application.LoadLevelAsync(scene);
 
