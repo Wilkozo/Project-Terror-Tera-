31aa4d6c43238837a7a33a66c853073d84eb1f85
Harry <profawesum@gmail.com>
2020-09-29 17:44:32 +1300

    Updates to door and other quality settings, Card swipe model
    Worked on animations and door opening, also made a model for the card swipe

--- Assets/Scripts/Puzzle/Doors.cs
+++ Assets/Scripts/Puzzle/Doors.cs
@@ -6,13 +6,17 @@ public class Doors : MonoBehaviour
 {
     
     public int levelToOpen;
-    
+
+    public Animator anim;
+    public Animator anim2;
+
     //when the door should be opened destroy it temp
     public void OpenDoor() {
         int temp = Keycards.getKeycardLevel();
         if(temp >= levelToOpen) {
             //SFX HERE
-            Destroy(this.gameObject);
+            anim.SetBool("Open", true);
+            anim2.SetBool("Open", true);
         }
     }
 }
